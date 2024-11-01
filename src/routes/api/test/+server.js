50d5cf8b3373c46ef4a3b7fe371b73198c5267b8
silicone-fig <me@siliconecb.cc>
2024-11-01 10:34:15 -0400

    a

--- src/routes/api/test/+server.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-// pulled from my tests at siliconecb.cc (portfolio site, check it out!)
-import { json } from '@sveltejs/kit';
-
-export async function GET() {
-  const fetch = await import('node-fetch').then(m => m.default);
-  const response = await fetch('https://wasteof.me/db/_/');
-  return json({ message: 'hello, world!' }, 200);
-}
