8fe325b553bbd640c603407c3b4785a61e906bee
silicone-fig <me@siliconecb.cc>
2024-11-11 19:22:16 -0500

    implement rate limiting

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 import { handler } from "./build/handler.js";
 import Koa from "koa";
 import dotenv from "dotenv";
+import { RateLimit } from "koa2-ratelimit";
 
 dotenv.config();
 
 const app = new Koa();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "0.0.0.0";
+
+// response handling (for rate limits)
+const errmsg = (ctx) => {
+  const now = Date.now();
+  const reset = ctx.state.rateLimit.reset * 1000;
+  const eta = Math.max(0, Math.ceil((reset - now) / 60000));
+
+  ctx.body = {
+    error: `bozo slow down, rate limited for: ${eta} minute${eta !== 1 ? 's' : ''}`
+  };
+  ctx.status = 429;
+};
+
+// rate limiter config
+const staticlimit = RateLimit.middleware({
+  interval: { min: 15 },
+  max: 99,
+  prefixKey: "static",
+  message: errmsg,
+  handler: errmsg
+});
+
+const apilimit = RateLimit.middleware({
+  interval: { min: 15 },
+  max: 100,
+  prefixKey: "api",
+  message: errmsg,
+  handler: errmsg
+});
+
+// rate limiter
+app.use(async (ctx, next) => {
+  if (ctx.path.startsWith('/static')) {
+    await staticlimit(ctx, next);
+  } else if (ctx.path.startsWith('/api')) {
+    await apilimit(ctx, next);
+  } else {
+    await next();
+  }
+});
 
 app.use(async (ctx, next) => {
   ctx.set('Via', 'varnish (Varnish/7.6.0)');
