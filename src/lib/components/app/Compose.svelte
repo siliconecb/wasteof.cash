938f246d516f6fbd1a5a8f867368d8d9317f8c01
silicone-fig <me@siliconecb.cc>
2025-01-02 22:13:42 -0500

    prod

--- src/lib/components/app/Compose.svelte
+++
@@ -1,7 +0,0 @@
-<div
-  class="rounded-3xl h-[130px] w-[540px] bg-[#1B1D1F] flex items-center justify-center text-center text-white"
->
-  <span style="font-family: '__nohemi_164e0f'; font-size: 14px;"
-    >coming soon!</span
-  >
-</div>
