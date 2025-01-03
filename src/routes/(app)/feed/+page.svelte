2fe77eed6497b541ed34595e11ad717141f1fbbb
silicone-fig <me@siliconecb.cc>
2025-01-02 23:43:04 -0500

    Revert "Merge branch 'production' of https://github.com/siliconecb/wasteof.cash into development"
    This reverts commit 2232b1a1c8b23b15cf479fdcb3bd30273e7dcb9a, reversing
    changes made to d1948d1c0f7844ce1e13f7fc6224558e326c46d8.

---
+++ src/routes/(app)/feed/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Feed from './Feed.svelte';
+</script>
+
+<svelte:head>
+  <title>feed | wasteof.cash</title>
+  <meta name="og:title" content="wasteof.cash - feed" />
+  <meta name="title" content="wasteof.cash - feed" />
+  <meta name="og:description" content="this is your feed, supercharged, decluttered, and personalized for you." />
+  <meta name="description" content="this is your feed, supercharged, decluttered, and personalized for you." />
+  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+</svelte:head>
+
+<Feed />
